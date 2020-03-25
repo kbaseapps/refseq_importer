924d3229f509df4644f4471a8ac45ff9395b835e
Jay R Bolton <jayrbolton@gmail.com>
2020-03-25 15:43:36 -0700

    Add code to get total counts in the refseq workspace

---
+++ stats/src/main.py
@@ -0,0 +1,23 @@
+import os
+import json
+from kbase_workspace_client import WorkspaceClient
+
+WS_TOK = os.environ['WS_TOK']
+WS_ID = int(os.environ.get('WS_ID', 15792))
+WS_URL = 'https://ci.kbase.us/services/ws'
+
+
+def main():
+    counts = {}  # type: dict
+    ws = WorkspaceClient(url=WS_URL, token=WS_TOK)
+    for obj_info in ws.generate_obj_infos(WS_ID, admin=True, latest=True):
+        obj_type = obj_info[2]
+        if obj_type not in counts:
+            counts[obj_type] = 0
+        counts[obj_type] += 1
+    print('Total counts by type:')
+    print(json.dumps(counts, indent=2))
+
+
+if __name__ == '__main__':
+    main()
